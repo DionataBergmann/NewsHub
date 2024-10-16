8248f9b80f7f18e81f9c778f331f2dc47cb4742a
DionataBergmann <dionata.podewils@outlook.com>
2024-10-16 17:14:56 -0300

    :sparkles: Implement general news fetching method

---
+++ src/services/newsService/index.tsx
@@ -0,0 +1,79 @@
+import axios from 'axios';
+
+const API_URL = process.env.NEXT_PUBLIC_NEWS_API_URL;
+const API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY;
+export const getGeneralNews = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/top-headlines`, {
+      params: {
+        country: 'us',
+        apiKey: API_KEY,
+      },
+    });
+
+    const filteredArticles = response.data.articles.filter(
+      (article) =>
+        article.author &&
+        article.title &&
+        article.description &&
+        article.urlToImage
+    );
+
+    return filteredArticles;
+  } catch (error) {
+    console.error('Erro ao buscar notícias:', error);
+    throw error;
+  }
+};
+
+export const getNewsByCategory = async (category: string) => {
+  try {
+    const response = await axios.get(`${API_URL}/top-headlines`, {
+      params: {
+        country: 'us',
+        category: category || 'General',
+        apiKey: API_KEY,
+      },
+    });
+
+    const filteredArticles = response.data.articles.filter(
+      (article) =>
+        article.author &&
+        article.title &&
+        article.description &&
+        article.urlToImage
+    );
+
+    return filteredArticles;
+  } catch (error) {
+    console.error(`Erro ao buscar notícias da categoria ${category}:`, error);
+    throw error;
+  }
+};
+
+export const searchNews = async (query: string) => {
+  try {
+    const response = await axios.get(`${API_URL}/everything`, {
+      params: {
+        q: query,
+        apiKey: API_KEY,
+      },
+    });
+
+    const filteredArticles = response.data.articles.filter(
+      (article) =>
+        article.author &&
+        article.title &&
+        article.description &&
+        article.urlToImage
+    );
+
+    return filteredArticles;
+  } catch (error) {
+    console.error(`Erro ao buscar notícias sobre: ${query}`, error);
+    throw error;
+  }
+};
+    throw error;
+  }
+};
