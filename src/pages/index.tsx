61b0e2fafc1ab2200871e944cd3f480f12c5c6d3
DionataBergmann <dionata.podewils@outlook.com>
2024-10-16 17:25:44 -0300

    :construction: Implement main news layout in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,57 @@
-import { Box } from '@chakra-ui/react';
-import Header from '../components/organisms/Header';
+import Header from '@/components/organisms/Header';
+import NewsLayout from '@/components/templates/NewsLayout';
+import { getGeneralNews, getNewsByCategory, searchNews } from '@/services/newsService';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>('General');
+  const [news, setNews] = useState([]);
+  const [newsByCategory, setNewsByCategory] = useState([]);
+
+  const handleCategoryChange = async (category: string) => {
+    setSelectedCategory(category);
+    try {
+      const categoryNews = await getNewsByCategory(category);
+      setNewsByCategory(categoryNews);
+    } catch (error) {
+      console.error('Erro ao carregar notícias por categoria', error);
+    }
+  };
+
+  const handleSearch = async (query: string) => {
+    if (query) {
+      try {
+        const searchResults = await searchNews(query);
+        setNewsByCategory(searchResults);
+      } catch (error) {
+        console.error('Erro ao buscar notícias:', error);
+      }
+    } else {
+      handleCategoryChange(selectedCategory);
+    }
+  };
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const categoryNews = await getNewsByCategory(selectedCategory);
+        setNewsByCategory(categoryNews);
+        const generalNews = await getGeneralNews();
+        setNews(generalNews);
+      } catch (error) {
+        console.error('Erro ao carregar notícias', error);
+      }
+    };
+
+    fetchNews();
+  }, [selectedCategory]);
+
   return (
-    <Box>
+    <>
       <Header />
-    </Box>
+
+      <NewsLayout selectedCategory={selectedCategory} news={news} />
+
+    </>
   );
 }
