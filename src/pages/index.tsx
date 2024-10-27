046a32c9ab3861bd5c187db3990496520194da50
DionataBergmann <dionata.podewils@outlook.com>
2024-10-27 14:28:52 -0300

    :ambulance: Fix build errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-// @ts-ignore
-import CategoryCarousel from '@/components/organisms/CategoryCarousel';
 import Header from '@/components/organisms/Header';
-import NewsList from '@/components/organisms/NewsList';
 import NewsLayout from '@/components/templates/NewsLayout';
 import { getGeneralNews, getNewsByCategory, searchNews } from '@/services/newsService';
 import { useEffect, useState } from 'react';
@@ -19,36 +16,10 @@ interface Article {
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState<string>('General');
   const [news, setNews] = useState<Article[]>([]); 
-  const [newsByCategory, setNewsByCategory] = useState<Article[]>([]); 
-
-  const handleCategoryChange = async (category: string) => {
-    setSelectedCategory(category);
-    try {
-      const categoryNews = await getNewsByCategory(category);
-      setNewsByCategory(categoryNews);
-    } catch (error) {
-      console.error('Erro ao carregar notícias por categoria', error);
-    }
-  };
-
-  const handleSearch = async (query: string) => {
-    if (query) {
-      try {
-        const searchResults = await searchNews(query);
-        setNewsByCategory(searchResults);
-      } catch (error) {
-        console.error('Erro ao buscar notícias:', error);
-      }
-    } else {
-      handleCategoryChange(selectedCategory);
-    }
-  };
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        const categoryNews = await getNewsByCategory(selectedCategory);
-        setNewsByCategory(categoryNews);
         const generalNews = await getGeneralNews();
         setNews(generalNews);
       } catch (error) {
@@ -57,17 +28,13 @@ export default function Home() {
     };
 
     fetchNews();
-  }, [selectedCategory]);
+  }, []);
 
   return (
     <>
       <Header />
-
       <NewsLayout selectedCategory={selectedCategory} news={news} />
 
-      <CategoryCarousel onSelectCategory={handleCategoryChange} onSearch={handleSearch} />
-
-      <NewsList news={newsByCategory} />
     </>
   );
 }
